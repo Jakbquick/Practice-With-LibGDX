5b07c14b8de3176d879b778a75d0804a4ae4a9ff
John Manard III <jakbquick2000@gmail.com>
2018-04-28 17:55:49 -0700

    added the bucket and allowed it to move within the frame

--- SimpleGame/core/src/com/badlogic/drop/Drop.java
+++ SimpleGame/core/src/com/badlogic/drop/Drop.java
@@ -2,31 +2,47 @@ package com.badlogic.drop;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector3;
 
 public class Drop extends ApplicationAdapter {
+	private Rectangle bucket;
 	private Texture dropImage;
 	private Texture bucketImage;
 	private Sound dropSound;
 	private Music rainMusic;
 	private SpriteBatch batch;
-	private Texture img;
+	private OrthographicCamera camera;
+	private Vector3 touchPos;
 	
 	@Override
 	public void create () {
-		img = new Texture(Gdx.files.internal("badlogic.jpg"));
+		//create camera
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 400);
 		//load images for droplet and bucket
 		dropImage = new Texture(Gdx.files.internal("droplet.png"));
 		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+
 		batch = new SpriteBatch();
+
 		//load images for sound effect and music
 		dropSound = Gdx.audio.newSound(Gdx.files.internal("drop.wav"));
 		rainMusic = Gdx.audio.newMusic(Gdx.files.internal("rain.mp3"));
+		//create rectangle to represent bucket
+		bucket = new Rectangle();
+		bucket.x = 800/2 - 64/2;
+		bucket.y = 20;
+		bucket.width = 64;
+		bucket.height = 64;
 
 		rainMusic.setLooping(true);
 		rainMusic.play();
@@ -34,16 +50,27 @@ public class Drop extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(bucketImage, bucket.x, bucket.y);
 		batch.end();
+		if(Gdx.input.isTouched()) {
+			touchPos = new Vector3();
+			touchPos.set(Gdx.input.getX(),Gdx.input.getY(), 0);
+			camera.unproject(touchPos);
+			bucket.x = touchPos.x - 64/2;
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
+		if(bucket.x < 0) bucket.x = 0;
+		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
 }
